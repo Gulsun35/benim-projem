7521133f996ce675fae2074704f343f6f12812c9
GulsunTuzun <77925175+GulsunTuzun@users.noreply.github.com>
2021-01-24 16:58:46 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(200)
+    basic.showString("Merhaba:)")
+    basic.pause(200)
+})
